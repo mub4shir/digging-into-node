8d2bf32f526f8dece2f0dca392180890d8d46fc0
mubashir <mubashir11131719@gmail.com>
2021-11-06 02:16:02 +0530

    done

--- digging-into-node/exercises/ex1.js
+++ digging-into-node/exercises/ex1.js
@@ -1,9 +1,55 @@
-// My first Node.js program
+#!/usr/bin/env node
 
-#! /usr/bin/env node
-//process.stdout.write('Hello world\n');
+'use strict';
 
-//process.stdin.read();
-console.log('Hello world');
+var path = require('path');
+var fs = require('fs');
 
-console.error('Oops');
+var getStdin = require('get-stdin');
+
+var args = require('minimist')(process.argv.slice(2), {
+  boolean: ['help', 'in'],
+  string: ['file'],
+});
+
+const BASEPATH = path.resolve(process.env.BASEPATH || __dirname);
+
+if (args.help || process.argv.length <= 2) {
+  error(null, /*showHelp=*/ true);
+} else if (args._.includes('-') || args.in) {
+  getStdin().then(processFile).catch(error);
+} else if (args.file) {
+  let filePath = path.join(BASEPATH, args.file);
+  fs.readFile(filePath, function (err, contents) {
+    if (err) error(err.toString());
+    else processFile(contents.toString());
+  });
+} else {
+  error('Usage incorrect.', /*showHelp=*/ true);
+}
+
+// ************************************
+
+function printHelp() {
+  console.log('ex1 usage:');
+  console.log('');
+  console.log('--help                      print this help');
+  console.log('-, --in                     read file from stdin');
+  console.log('--file={FILENAME}           read file from {FILENAME}');
+  console.log('');
+  console.log('');
+}
+
+function error(err, showHelp = false) {
+  process.exitCode = 1;
+  console.error(err);
+  if (showHelp) {
+    console.log('');
+    printHelp();
+  }
+}
+
+function processFile(text) {
+  text = text.toUpperCase();
+  process.stdout.write(text);
+}

b09f09f0569c054bf75ec4fc0f2d047695aee497
mubashir <mubashir11131719@gmail.com>
2021-11-06 20:43:45 +0530

    child processess

--- digging-into-node/exercises/ex7-child.js
+++ digging-into-node/exercises/ex7-child.js
@@ -1,18 +1,26 @@
-"use strict";
+'use strict';
 
-// var fetch = require("node-fetch");
-
+var fetch = require('node-fetch');
 
 // ************************************
 
 const HTTP_PORT = 8039;
 
-
 main().catch(() => 1);
-
 
 // ************************************
 
 async function main() {
-	// TODO
+  try {
+    var res = await fetch(`http://localhost:${HTTP_PORT}/get-records`);
+    if (res && res.ok) {
+      let records = await res.json();
+      if (records && records.length > 0) {
+        process.exitCode = 0;
+        return;
+      }
+    }
+  } catch (err) {}
+
+  process.exitCode = 1;
 }
